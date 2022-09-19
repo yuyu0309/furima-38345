b1cd58eb56bceeffd99a8e566a9d075c61e1f816
yuyu0309 <yulego@i.softbank.jp>
2022-09-19 21:55:08 +0900

    テストコードの実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    item_name { '商品名' }
+    explanation { '商品の説明' }
+    category_id { '2' }
+    situation_id { '3' }
+    shipping_charge_id { '3' }
+    prefecture_id { '10' }
+    delivery_date_id { '3' }
+    price { '612' }
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/star.png'), filename: 'star.png')
+    end
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,5 @@
 FactoryBot.define do
   factory :user do
-    #transient do
-    #  person { Gimei.name }
-    #end
     nickname              {Faker::Name.initials(number: 2)}
     email                 {Faker::Internet.free_email}
     password              {Faker::Lorem.characters(number:6, min_alpha: 1, min_numeric: 1)}

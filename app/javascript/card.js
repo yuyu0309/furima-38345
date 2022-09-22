328d0045c22538f12cc2fbb78168885541a1e8c8
yuyu0309 <yulego@i.softbank.jp>
2022-09-22 12:33:32 +0900

    console.logの削除

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,6 +1,5 @@
 const pay = () => {
   Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
-  console.log(process.env.PAYJP_PUBLIC_KEY)
   const submit = document.getElementById("button");
   submit.addEventListener("click", (e) => {
     e.preventDefault();
